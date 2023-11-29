3718acf590c9e1fe605523c0d531504f58d95e23
FoxCityFoxes <152361338+FoxCityFoxes@users.noreply.github.com>
2023-11-28 23:13:43 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -24,7 +24,7 @@ export const managers = [
     {
       // "roster": 3,  // (DEPRECATED! Don't use this anymore) ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID": "432304383527505920",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
-      "name": "Colin",
+      "name": "Gruden's Grinders 69",
       "tookOver": null, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Tommy DeVito's Hometown", // (optional)
       "bio": "A guy's guy. Summers on the shore. Yachts. Never uses his given name",
